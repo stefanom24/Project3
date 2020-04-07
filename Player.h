bf91b325fb40081c6f5ea49ce4ca9dbfcfe9da5b
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-07 13:01:42 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,23 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+#include<string>
+using namespace std;
 
+class Player
+{
+    private:
+   
+    string playerName;
+    
+   
+    public:
+    
+    Player();
+    Player(string);
+   
+    string getPlayerName();
+    void setPlayerName(string);
+    
+};
+
+#endif
