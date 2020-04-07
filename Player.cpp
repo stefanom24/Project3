8a56b47fea9ec9d96e55bf537c731373e6e4faf8
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-07 13:02:41 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+
+#include <string>
+
+#include "Player.h"
+
+using namespace std;
+
+Player::Player()
+{
+    title = "";
+    author = "";
+}
+
+Book::Book(string new_title, string new_author)
+{
+    title = new_title;
+    author = new_author;
+}
+
+
+string Book::getTitle()
+{
+    return title;
+}
+string Book::getAuthor()
+{
+    return author;
+}
+
+
+void Book :: setTitle(string new_title)
+{
+    title = new_title;
+}
+void Book :: setAuthor(string new_author)
+{
+    author = new_author;
+}
 
