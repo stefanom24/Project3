9a92d58ae111720115cc0d94d9c4c8d106b2f5d6
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-07 12:46:15 -0600

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,1 +1,21 @@
+#ifndef DATE_HPP
+#define DATE_HPP
+#include<string>
+using namespace std;
 
+class Date
+{
+    private:
+   
+    int currentDate;
+    
+   
+    public:
+    
+    Date();
+    Date(int);
+   
+    int getDate();
+};
+
+#endif
