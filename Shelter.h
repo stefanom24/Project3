ca2aa7b996ffb197cc975be8be5e1855da534562
stefanom24 <63312971+stefanom24@users.noreply.github.com>
2020-04-07 13:10:20 -0600

    Update Shelter.h

--- Shelter.h
+++ Shelter.h
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "Events.h"
+using namespace std;
 
+#ifndef SHELTER_H
+#define SHELTER_H
+
+class Shelter{
+  private:
+    int locationID;
+    int defensePoints;
+    int forageEase;
+  
+  public:
+    Shelter();
+    Shelter(int);
+  
+    getdefensePoints();
+    getforageEase();
+  
+    setdefensePoints(int);
+    setforageEase(int);
+};
+#endif
