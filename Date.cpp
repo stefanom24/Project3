05a97c9e9db7ad12023e6f63cd14882d96edb73b
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-07 12:56:34 -0600

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+#include <string>
+
+#include "Date.h"
+
+using namespace std;
+
+Date::Date()
+{
+    currentDay = 1
+    //year,month,day?
+}
+
+Date::Date(int currentDay)
+{
+    //??
+}
+
+
+int Date::getDate()
+{
+    return currentDay;
+}
+
 
