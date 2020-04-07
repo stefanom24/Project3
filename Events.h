9d0f48be743b2ba16b3656bd59c0635408eb0bb5
stefanom24 <63312971+stefanom24@users.noreply.github.com>
2020-04-07 13:00:16 -0600

    Update Events.h

--- Events.h
+++ Events.h
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "Date.h"
+using namespace std;
 
+#ifndef EVENTS_H
+#define EVENTS_H
+
+class Events{
+  private:
+    int currentDay;
+    int locationID;
+  public: 
+    Events();
+    Events(int, int);
+  
+    setcurrentDay(int);
+    setlocationID(int);
+  
+    getcurrentDay();
+    getlocationID();
+  
+};
+#endif
