fd3b46ee48043c9e816a2368994028fb31ebf2eb
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-08 16:00:40 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -9,7 +9,12 @@ using namespace std;
 Player::Player()
 {
     playerName = "";
-    
+    int supplySize = 0;
+    for (int i = 0; i<15; i++) //initilizes user/player supplies array to 0
+    {
+        userSupplies[i] = 0;
+        userSupplies++;
+    }
 }
 
 Player::Player(string new_player)
