aae251ae2ba50ccafbfa8b9b5e492707a03ebf77
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-08 16:06:26 -0600

    Update Enemies.h

--- Enemies.h
+++ Enemies.h
@@ -1,2 +1,30 @@
 // different types and difficulty levels. 
 // randomize which aliens appear in each day. more than one?
+#ifndef ENEMIES_HPP
+#define ENEMIES_HPP
+#include<string>
+
+using namespace std;
+
+class Enemies
+{
+    private:
+   
+    string enemieName;
+    int enemieStats[3];
+    
+   
+    public:
+    
+    Enemie();
+    Enemie(string);
+   
+    string getEnemiename();
+    int getStatsAt(int);
+   
+    void setEnemiename(string);
+    bool setStatesAt(int , int); //takes an index, rating value
+    
+};
+
+#endif
