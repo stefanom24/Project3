c6eca575fbff568dc64fe13eadf496201e9465c2
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-16 23:00:10 -0600

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -9,51 +9,54 @@
 #include<string>
 
 
-
-using namespace std;
-
-
-
-class Enemies
+Enemies :: Enemies()
 
 {
 
     private:
 
+    string enemyName = "";
+
+    int statSize = 0;
+    for (int i = 0; i<15; i++) //initilizes enemie stats array to 0
+    {
+        enemyStats[i] = 0;
+        enemyStats++;
+    }
+    
+}
+
+
+Enemies :: Enemies(string new_Enemy)
+{
+    enemyName = new_Enemy;
+}
+
    
 
-    string enemieName;
+string Enemies::getEnemyname(string )
+{
+    
+}
 
-    int enemieStats[3];
 
+int Enemies:: getStatsAt(int)
+{
+    
+}
+
+
+void Enemies:: setEnemyname(string)
+{
+    
+}
+
+bool Enemies:: setStatesAt(int , int) //takes an index, rating value
+{
+    
+}
     
 
-   
-
-    public:
-
-    
-
-    Enemie();
-
-    Enemie(string);
-
-   
-
-    string getEnemiename();
-
-    int getStatsAt(int);
-
-   
-
-    void setEnemiename(string);
-
-    bool setStatesAt(int , int); //takes an index, rating value
-
-    
-
-};
 
 
 
-#endif
