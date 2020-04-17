8912bef15671b30ab2d3f9bd35578cdbd6e29f7f
stefanom24 <63312971+stefanom24@users.noreply.github.com>
2020-04-16 19:19:17 -0600

    Update Shelter.cpp

--- Shelter.cpp
+++ Shelter.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 Shelter::Shelter(){
   shelterID = 0;
-  defensePoints = 0;
-  suppliesAmount = 0;
+  defensePoints = 0; //Probability of raid events. Low defense = 50%, Medium defense = 25%, High defense = 10%.
+  suppliesAmount = 0; //Costs 7 supplies a day to survive, need total of 49 supplies to survive the seven days.
 }
 
 Shelter::Shelter(int input_){
   int input = input_;
   if(input == 1){
-    setshelterID(1);
-    setdefensePoints(50);
-    setsuppliesAmount(28);
+    setshelterID(1); //Friend's apartment in the city.
+    setdefensePoints(50); //Low defense.
+    setsuppliesAmount(28); //High supplies.
   }
   else if(input == 2){
-    setshelterID(2);
-    setdefensePoints(25);
-    setsuppliesAmount(14);
+    setshelterID(2); //Campsite in mountains/
+    setdefensePoints(25); //Medium defense.
+    setsuppliesAmount(14); //Medium supplies.
   }
   else if(input == 3){
-    setshelterID(3);
-    setdefensePoints(10);
-    setsuppliesAmount(35);
+    setshelterID(3); //Bunker, if found.
+    setdefensePoints(10); //High defense.
+    setsuppliesAmount(35); //Ultra high supplies.
   }
 }
 
