32324e3dd0b1368af384cd112fe4cb7e87801f9e
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-16 23:23:54 -0600

    Update Events.cpp

--- Events.cpp
+++ Events.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include "Events.h"
+using namespace std;
+
+#ifndef EVENTS_H
+#define EVENTS_H
+
+class Events{
+  private:
+    int currentDay;
+    int locationID;
+    
+  public: 
+    Events();
+    Events(int, int);
+  
+    setcurrentDay(int);
+    setlocationID(int);
+  
+    getcurrentDay();
+    getlocationID();
+  
+};
+#endif
