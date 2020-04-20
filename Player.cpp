2be6b6a1848244e7c5b02c9f5bfe278074357674
stefanom24 <63312971+stefanom24@users.noreply.github.com>
2020-04-19 20:37:29 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -10,6 +10,7 @@ Player::Player()
 {
     playerName = "";
     int supplySize = 0;
+    playerHealth = 100;
     for (int i = 0; i<15; i++) //initilizes user/player supplies array to 0
     {
         userSupplies[i] = 0;
@@ -34,3 +35,14 @@ void Player :: setPlayerName(string new_player)
 {
     playerName = new_player;
 }
+
+void Player::setPlayerHealth(int dmgInput_){
+    int dmgInput = dmgInput_;
+    int newHealth = playerHealth - dmgInput;
+    playerHealth = newHealth;
+}
+
+int Player::getPlayerHealth(){
+    return playerHealth;
+}
+
