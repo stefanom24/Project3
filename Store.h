93ddd3fbb5af4743367f9b416006039ba22a80ac
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-22 22:34:08 -0600

    Update Store.h

--- Store.h
+++ Store.h
@@ -1,3 +1,41 @@
 // use as inventory
 // fixed array of size ?, for weapons/food.
 //foraged items/foraging will come from here
+#include <iostream>
+
+
+	//only carry one weapon 
+	//healing items
+#ifndef STORE_HPP
+#define STORE_HPP
+#include<string>
+
+using namespace std;
+
+class Store
+{
+    private:
+   
+    string weaponsAvail[3];
+    string userWeapon;
+    int numMedical;
+    
+    
+    public:
+    
+    Store();
+   
+    int readWeapons(string);
+    string getUserWeaponName(int);
+    
+    
+    void viewAllItems(string, int);
+    void printWeaponName();
+    void updateWeapon(string, string, int);
+    void updateMedical(int, int);
+    
+};
+
+#endif
+	
+	
