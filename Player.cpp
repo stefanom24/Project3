c67a6f1d050a2417be623e2bd0292f1881d9b2fa
maciestrick9 <63312988+maciestrick9@users.noreply.github.com>
2020-04-23 13:11:54 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,48 +1,113 @@
 #include <iostream>
 
+
+
 #include <string>
+
+
 
 #include "Player.h"
 
+
+
 using namespace std;
 
+
+
 Player::Player()
+
 {
+
     playerName = "";
+
     //int supplySize = 0;
+
     playerHealth = 100;
-//     for (int i = 0; i<15; i++) //initilizes user/player supplies array to 0
-//     {
-//         userSupplies[i] = 0;
-//         userSupplies++;
-//     }
+    weaponName = "";
+    playerMedSupplies = 0;
+
+ 
+
 }
 
+
+
 Player::Player(string new_player)
+
 {
+
     playerName = new_player;
+
+}
+
+
+string Player::getWeaponName()
+{
+    return weaponName
 }
 
 
 string Player::getPlayerName()
+
 {
+
     return playerName;
+
+}
+
+int Player::getPlayerMedSupplies()
+{
+    return playerMedSupplies;
 }
 
 
-
-void Player :: setPlayerName(string new_player)
+void Player::setPlayerMedSupplies(int change, int IorD)//takes in change amount and if incre or decre
 {
+    if (IorD == 0)
+    {
+        //decrease
+        playerMedSupplies = playerMedSupplies - change;
+    }
+    else if (IorD == 1)
+    {
+        playerMedSupplies = playerMedSupplies + change;
+    }
+    else
+    {
+        cout << "Invalid Option." << endl;
+    }
+    
+}
+void Player :: setPlayerName(string new_player)
+
+{
+
     playerName = new_player;
+
+}
+
+void Player :: setWeaponName(string new_weapon)
+{
+    weaponName = new_weapon;
 }
 
 void Player::setPlayerHealth(int dmgInput_){
+
     int dmgInput = dmgInput_;
-    int newHealth = playerHealth - dmgInput;
-    playerHealth = newHealth;
+
+    
+
+    playerHealth = dmgInput;
+
+}
+
+void Player::setPlayerHealing(int heal)
+{
+    playerHealth = playerHealth + heal;
 }
 
 int Player::getPlayerHealth(){
-    return playerHealth;
-}
 
+    return playerHealth;
+
+}
